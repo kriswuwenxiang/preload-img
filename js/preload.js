2e631a9e2c0eb8c11913798d2707e59e16b01a63
wuwenxiang <794783332@qq.com>
2018-04-07 18:35:12 +0800

    预加载（无序/有序）

--- js/preload.js
+++ js/preload.js
@@ -3,52 +3,53 @@
     this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
     this.opts = $.extend({}, PreLoad.DEFAULTS, options);
 
-    if (this.opts.order === "ordered") {
-      this._ordered()
+    if (this.opts.type === "order") { //判断加载类型
+      this._order()
     } else {
-      this._unordered();
+      this._unorder();
     }
   }
   PreLoad.DEFAULTS = {
-    order: 'unordered', //无序预加载
-    each: null, //每张图片加载完毕后执行
-    all: null //所有图片加载完后执行
+    type: 'unorder', //无序预加载
+    eachfun: null, //每张图片加载完毕后执行
+    allfun: null //所有图片加载完后执行
   };
-  PreLoad.prototype._unordered = function(){   //无序加载
+
+  PreLoad.prototype._unorder = function(){ //无序加载
     var imgs = this.imgs,
         opts = this.opts,
         count = 0,
         len = imgs.length;
-    $.each(imgs, function(i, src) {
-      if (typeof src != 'string') {
+
+    $.each(imgs, function(index, src) {
+      if (typeof src !== 'string') {
         return;
       }
       var imgObj = new Image();
       $(imgObj).on('load error', function() {
-        opts.each && opts.each(count);
-        if (count >= len - 1) {
-          opts.all && opts.all();
+        opts.eachfun && opts.eachfun(count);
+        if (count >= len - 1) { //所有图片已经加载完毕
+          opts.allfun && opts.allfun();
         }
         count++;
       });
       imgObj.src = src;
     });
   };
-  PreLoad.prototype._ordered = function() {
+  
+  PreLoad.prototype._order = function() { //有序加载
     var opts = this.opts,
         imgs = this.imgs,
         len = imgs.length,
         count = 0;
 
         load();
-
         function load() {
           var imgObj = new Image();
           $(imgObj).on('load error', function() {
-              opts.each && opts.each(count);
-              if (count >= len) {
-                //所有图片已经加载完毕
-                opts.all && opts.all();
+              opts.eachfun && opts.eachfun(count);
+              if (count >= len) { //所有图片已经加载完毕
+                opts.allfun && opts.allfun();
               } else {
                 load();
               }
@@ -57,6 +58,7 @@
           imgObj.src = imgs[count];
         }
   }
+
   $.extend({
     preload: function(imgs, opts) {
       new PreLoad(imgs, opts);
